027123603df5781f947907a6ff0f333bc2494a0a
Xinyu Wang <xinyu.wang@uconn.edu>
2018-11-29 14:17:18 -0500

    update

--- main.py
+++ main.py
@@ -15,8 +15,19 @@ print('Completed!')
 
 
 
+
+
+
+
 print('Locate and replace Caps Lock Configuration in xkbmap:', end='')
 key_word = 'key <CAPS>'
+replace = '''key <CAPS> {
+    repeat=no,
+    type[group1]="ALPHABETIC",
+    symbols[group1]=[ Caps_Lock, Caps_Lock],
+    actions[group1]=[ LockMods(modifiers=Lock), Private(type=3,data[0]=1,data[1]=3,data[2]=3)]
+};'''
+
 
 f = open('xkbmap', 'r')
 lines = f.readlines()
@@ -28,10 +39,13 @@ len_lines = len(lines)
 while i < len_lines:
 	line = lines[i]
 	if key_word in line:
-		lines[i+1] = '''	repeat=no,'''
-		lines[i+2] = '''	type[group1]="ALPHABETIC",'''
-		lines[i+3] = '''	symbols[group1]=[ Caps_Lock, Caps_Lock],'''
-		lines[i+4] = '''	actions[group1]=[ LockMods(modifiers=Lock), Private(type=3,data[0]=1,data[1]=3,data[2]=3)]'''
+		lines[i] = replace
+#		for j in range(10):
+#			print('current:', lines[i+j])
+#		lines[i+1] = '''	repeat=no,'''
+#		lines[i+2] = '''	type[group1]="ALPHABETIC",'''
+#		lines[i+3] = '''	symbols[group1]=[ Caps_Lock, Caps_Lock],'''
+#		lines[i+4] = '''	actions[group1]=[ LockMods(modifiers=Lock), Private(type=3,data[0]=1,data[1]=3,data[2]=3)]'''
 		break
 	i += 1
 	
